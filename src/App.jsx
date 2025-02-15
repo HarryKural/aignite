dc1db3cff78af327508d620b7be0936de789b6d4
harrykural <harrykural333@gmail.com>
2025-02-14 19:04:12 -0500

    add: implement login page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from "./pages/Services";
 import Booking from "./pages/Booking";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/booking" element={<Booking />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+
+  return (
+    <section className="flex items-center justify-center px-6 mt-15">
+      
+    </section>
+  );
+}
